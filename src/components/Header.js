ee51be5ec366584292116eaa349b493833418b85
Fuad Talybov <fuad.talybov@gmail.com>
2024-05-19 15:22:24 -0600

    updated resume data

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 function Header({author}) {
 
   return (
-    <header id="home" className="pt-20 mt-20 mb-20 text-black ">
-      <div className="w-auto md:w-3/4">
+    <header id="home" className="pt-20 mt-20 mb-5 text-black ">
+      <div className="w-auto md:w-full lg:w-3/4 mb-5">
 
             <h1 className="author-name text-gray-900">
               {author.name}
             </h1>
+            <span className="author-spelling text-gray-500">
+              {author.spelling}
+            </span>
       </div>
       <div className="md:w-3/4" >
             <p className="author-description pt-0 text-gray-700">
